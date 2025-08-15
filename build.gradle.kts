1cafb0903e6b636053695bd6efca6c92c9cca608
mayonnaise <tranquilham@gmail.com>
2025-08-14 21:10:13 -0400

    FUNI

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ java {
 
 // Most RFG configuration lives here, see the JavaDoc for com.gtnewhorizons.retrofuturagradle.MinecraftExtension
 minecraft {
+
     mcVersion.set("1.7.10")
 
     // Username for client run configurations
@@ -57,7 +58,7 @@ tasks.injectTags.configure {
 // Put the version from gradle into mcmod.info
 tasks.processResources.configure {
     val projVersion = project.version.toString() // Needed for configuration cache to work
-    inputs.property("version", projVersion)
+    inputs.property("modVersion", projVersion)
 
     filesMatching("mcmod.info") {
         expand(mapOf("modVersion" to projVersion))
@@ -96,6 +97,7 @@ repositories {
 dependencies {
     // Adds NotEnoughItems and its dependencies (CCL&CCC) to runClient/runServer
     runtimeOnlyNonPublishable("com.github.GTNewHorizons:NotEnoughItems:2.3.39-GTNH:dev")
+    implementation("org.scala-lang:scala-actors:2.10.0-M7")
     // Example: grab the ic2 jar from curse maven and deobfuscate
     // api(rfg.deobf("curse.maven:ic2-242638:2353971"))
     // Example: grab the ic2 jar from libs/ in the workspace and deobfuscate
